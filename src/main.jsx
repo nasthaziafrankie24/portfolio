bd49f779662936d7577a3ed8551ae2526d2aab85
Nasthazia <nasthaziafrankie24@gmail.com>
2026-04-23 02:07:48 +0530

    installed material icon

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import 'material-symbols';
 
 createRoot(document.getElementById('root')).render(
     <App />
